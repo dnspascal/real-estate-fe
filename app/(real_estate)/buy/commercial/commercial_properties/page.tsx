db8e7d547c0e93e3f5656dc4beff74fabcb2aeae
dnspascal <dnspascal094@gmail.com>
2023-09-12 19:30:32 +0300

    putting the condtion on map for specific properties

--- app/(real_estate)/buy/commercial/commercial_properties/page.tsx
+++ app/(real_estate)/buy/commercial/commercial_properties/page.tsx
@@ -7,21 +7,33 @@ import { useSelector } from 'react-redux'
 const Page = () => {
     const specificProperties = useSelector((state: any) => state.specificProperties.items)
 
-    
+
     return (
         <main className='p-6'>
             <span>All available
                 <span className='font-bold'>
                     {specificProperties[0]?.type}</span>
+
+
             </span>
 
 
             <div className='flex flex-col sm:flex-row flex-wrap items-center h-full gap-y-8 sm:justify-around' >
                 {
-                    specificProperties?.map((item: { name: string; location: string; price: number; imgUrl: string; published: string; utilities: string[] }, index: React.Key | null | undefined) =>
-                        <PropertyItem item={item} key={index} />)}
+                    specificProperties?.length > 0 ? (
+
+                        specificProperties?.map((item: { name: string; location: string; price: number; imgUrl: string; published: string; utilities: string[] }, index: React.Key | null | undefined) =>
+                            <PropertyItem item={item} key={index} />
+                        )) : (
+
+                        <div>here</div>
+                    )
+
+                }
+
             </div>
-        </main>)
+        </main>
+    )
 }
 
 export default Page
